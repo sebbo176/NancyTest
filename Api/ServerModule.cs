718f620c1001835708cdeeee4f07d5805c3011ec
Sebastian Bolling <sebastian.bolling@gmail.com>
2015-01-31 22:37:12 +0100

    changed routing

---
+++ Api/ServerModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Api
+{
+    public class ServerModule : NancyModule
+    {
+        public ServerModule()
+        {
+            Get["/"] = p => "Hi there";
+        }
+    }
+}
