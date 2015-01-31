a3869debccd39cd2f44ba00869ba4a1371d9fbfa
Sebastian Bolling <sebastian.bolling@gmail.com>
2015-01-31 21:14:33 +0100

    Initial checkin with API

---
+++ Api/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Api
+{
+    public class Server : NancyModule
+    {
+        public Server()
+        {
+            Get["/Api/Users"] = p => "Hejsan världen";
+        }
+    }
+}
