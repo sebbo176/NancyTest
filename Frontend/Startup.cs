34c8ffb9ca8639957ea4f364704344e33b1d4181
Sebastian Bolling <sebastian.bolling@gmail.com>
2015-02-01 11:09:35 +0100

    added more files after build

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Owin;
 
 namespace Frontend
 {
     public class Startup
     {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
     }
 }
