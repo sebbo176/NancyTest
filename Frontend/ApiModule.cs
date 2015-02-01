8c8fda7f0ebddbf8da90f326b29f0898a9d11b52
Sebastian Bolling <sebastian.bolling@gmail.com>
2015-02-01 11:18:18 +0100

    added webproject

---
+++ Frontend/ApiModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Nancy;
+
+namespace Frontend
+{
+    public class ApiModule : NancyModule
+    {
+        public ApiModule()
+        {
+            Get["/Hello"] = parameters => "Hi there! :)";
+            Get["/"] = p => "Battlestation operational";
+        }
+    }
+}
