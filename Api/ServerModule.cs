beac91457ee38d5535a89eb21338c7adf119567e
Sebastian Bolling <sebastian.bolling@gmail.com>
2015-02-01 11:08:29 +0100

    added new projects

---
+++ Api/ServerModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Api
+{
+    public class ServerModule : NancyModule
+    {
+        public ServerModule()
+        {
+            Get["/Hello"] = parameters => "Hi there! :)";
+        }
+    }
+}
