89c24a682d932f0b862f5a20c9d91f42f45f5216
Sebastian Bolling <sebastian.bolling@gmail.com>
2015-02-01 10:02:10 +0100

    used a webproject instead

---
+++ Server/ApiModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Server
+{
+    public class ApiModule: NancyModule
+    {
+        public ApiModule()
+        {
+            Get["/"] = p => "Hi there";
+        }
+    }
+}
